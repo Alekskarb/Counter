ad3aef11b90d08d92632bebf7814feaad7ceb17f
sasha <korbutowski@gmail.com>
2019-11-01 13:33:08 +0300

    Updates

---
+++ precache-manifest.1d7a98e8d39eff31f6ba440515dd1a5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85061fb4008efefaeccd9c748ce357f8",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "b1d3545237f9d2e41a70",
+    "url": "/counter/static/css/main.ebb1e6ec.chunk.css"
+  },
+  {
+    "revision": "28d5c1680e2ef375f6d6",
+    "url": "/counter/static/js/2.83518046.chunk.js"
+  },
+  {
+    "revision": "b1d3545237f9d2e41a70",
+    "url": "/counter/static/js/main.6eb53e5b.chunk.js"
+  },
+  {
+    "revision": "6d678bcc71c3aa331f85",
+    "url": "/counter/static/js/runtime~main.3e4aed89.js"
+  }
+]);
